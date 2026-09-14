a770be187d24563b573cc8a9356b6d38423c0f23
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-09-14 14:50:14 +0200

    add weather and news response types

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface WeatherData {
+  latitude: number;
+  longitude: number;
+
+  current: {
+    temperature_2m: number;
+    wind_speed_10m: number;
+    weather_code: number;
+  };
+
+  current_units: {
+    temperature_2m: string;
+    wind_speed_10m: string;
+    weather_code: string;
+  };
+}
+
+export interface NewsPost {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface NewsData {
+  posts: NewsPost[];
+  total: number;
+  skip: number;
+  limit: number;
+}
