d258784440616581885c83202cc254d822b9fc48
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-09-16 14:46:52 +0200

    add switch statement to convert weather code to weather condition

--- src/api.ts
+++ src/api.ts
@@ -152,50 +152,37 @@ export function getWeatherDescription(
     case 3:
       return "Overcast";
 
-    case 45:
-    case 48:
+    case 45: case 48:
       return "Fog";
 
-    case 51:
-    case 53:
-    case 55:
+    case 51: case 53: case 55:
       return "Drizzle";
 
-    case 56:
-    case 57:
+    case 56: case 57:
       return "Freezing drizzle";
 
-    case 61:
-    case 63:
-    case 65:
+    case 61: case 63: case 65:
       return "Rain";
 
-    case 66:
-    case 67:
+    case 66: case 67:
       return "Freezing rain";
 
-    case 71:
-    case 73:
-    case 75:
+    case 71: case 73: case 75:
       return "Snow";
 
     case 77:
       return "Snow grains";
 
-    case 80:
-    case 81:
-    case 82:
+    case 80: case 81: case 82:
       return "Rain showers";
 
-    case 85:
-    case 86:
+    case 85: case 86:
       return "Snow showers";
 
     case 95:
       return "Thunderstorm";
 
-    case 96:
-    case 99:
+    case 96:  case 99:
       return "Thunderstorm with hail";
 
     default:
